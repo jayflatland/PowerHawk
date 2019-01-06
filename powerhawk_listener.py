ac2e9bbc1844b4a3dfe86632e5155832dc0a953f
Jay Flatland <jayflatland@gmail.com>
2019-01-06 16:44:10 -0600

    Set up multicast

---
+++ powerhawk_listener.py
@@ -0,0 +1,19 @@
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the port
+server_address = ('', 10243)
+print('starting up on %s port %s' % server_address)
+sock.bind(server_address)
+
+while True:
+    # print('\nwaiting to receive message')
+    data, address = sock.recvfrom(4096)
+    #print(data)
+    msg = data.decode('utf-8')
+
+    parts = [float(v) for v in msg.split(',')]
+    print(parts)
--- sketch_dec23a/sketch_dec23a.ino
+++ sketch_dec23a/sketch_dec23a.ino
@@ -7,6 +7,7 @@ const char * networkPswd = "deadbeef";
 
 unsigned long scheduled_next_tick = 0;
 unsigned long last_report_us = 0;
+unsigned long last_tx_us = 0;
 
 int ref_pin = 36;
 int in1_pin = 39;
@@ -53,8 +54,6 @@ void connectToWiFi(const char * ssid, const char * pwd)
     Serial.println("WiFi connected!");
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
-
-    //Udp.begin(12345);
 }
 
 
@@ -111,163 +110,39 @@ void loop()
 
     hist_idx = (hist_idx + 1) % HIST_CNT;
 
-    //in1d2_sum *= 0.9999;  //hack to make numerically stable.  don't accum float rounding error.
-    // float in1d_rms = sqrt(in1d2_sum / HIST_CNT);
-    // if(in1d2_sum < 0.0) {
-    //     in1d_rms = 0.0;
-    // }
-
-    // //float amps = (in1d_rms - 8.0) * 0.32;
-    // if(amps < 0.0) {
-    //     amps = 0.0;
-    // }
-
-    // //in1d_filt2 = in1d_filt2 * 0.995 + in1d * in1d * 0.005;
-    // //float in1d_filt = sqrt(in1d_filt2);
+    float reading_to_amps = 1.0 / 15.0;
+    float in1_rms = in1_sq_sum > 0 ? sqrt((float)(in1_sq_sum / HIST_CNT)) * reading_to_amps : 0.0;
+    float in2_rms = in2_sq_sum > 0 ? sqrt((float)(in2_sq_sum / HIST_CNT)) * reading_to_amps : 0.0;
+    float in3_rms = in3_sq_sum > 0 ? sqrt((float)(in3_sq_sum / HIST_CNT)) * reading_to_amps : 0.0;
+    float in4_rms = in4_sq_sum > 0 ? sqrt((float)(in4_sq_sum / HIST_CNT)) * reading_to_amps : 0.0;
 
     ///////////////////////////////////////////////////////////////////////////
     // REPORTING
     ///////////////////////////////////////////////////////////////////////////
     if(1) {  // raw heart signal diagnostics
         if((long)(now - last_report_us) > 0) {
-            float in1_rms = in1_sq_sum > 0 ? sqrt((float)(in1_sq_sum / HIST_CNT)) : 0.0;
-            float in2_rms = in2_sq_sum > 0 ? sqrt((float)(in2_sq_sum / HIST_CNT)) : 0.0;
-            float in3_rms = in3_sq_sum > 0 ? sqrt((float)(in3_sq_sum / HIST_CNT)) : 0.0;
-            float in4_rms = in4_sq_sum > 0 ? sqrt((float)(in4_sq_sum / HIST_CNT)) : 0.0;
-
             last_report_us = now;
             Serial.print(in1_rms);Serial.print(",");
             Serial.print(in2_rms);Serial.print(",");
             Serial.print(in3_rms);Serial.print(",");
             Serial.print(in4_rms);Serial.println();
-            //Serial.print(ref);Serial.println();
-            // Serial.print(100.0*in1n);Serial.print(",");
-            // Serial.print(in1d_rms);Serial.print(",");
-            // Serial.print(in1d);Serial.println();
+        }
+    }
+
+    if(1) {  // raw heart signal diagnostics
+        if((long)(now - last_tx_us) > 1000000) {
+            last_tx_us = now;
+            String msg = String(in1_rms) + "," + \
+                         String(in2_rms) + "," + \
+                         String(in3_rms) + "," + \
+                         String(in4_rms);
 
             // Serial.print(amps);Serial.println();
-            // Udp.beginPacket("10.1.10.255", 12345);
-            // unsigned char buf[] = "hi from jay...\r\n";
-            // Udp.write(buf, sizeof(buf));
-            // Udp.endPacket();
+            Udp.beginPacket("10.1.10.255", 10243);
+            //unsigned char buf[] = "hi from jay...\r\n";
+            //Udp.write(buf, sizeof(buf));
+            Udp.write((const uint8_t*)msg.c_str(), msg.length());
+            Udp.endPacket();
         }
     }
 }
-
-//#include <WiFi.h>
-//
-//// WiFi network name and password:
-//const char * networkName = "jaysplace2";
-//const char * networkPswd = "deadbeef";
-//
-//// Internet domain to request from:
-//const char * hostDomain = "google.com";
-//const int hostPort = 80;
-
-//const int BUTTON_PIN = 0;
-//const int LED_PIN = 5;
-
-//void setup()
-//{
-//  // Initilize hardware:
-//  Serial.begin(115200);
-//  //pinMode(BUTTON_PIN, INPUT);
-//  //pinMode(LED_PIN, OUTPUT);
-//
-//  // Connect to the WiFi network (see function below loop)
-//  //connectToWiFi(networkName, networkPswd);
-//
-//  //digitalWrite(LED_PIN, LOW); // LED off
-//  //Serial.print("Press button 0 to connect to ");
-//  //Serial.println(hostDomain);
-//}
-//
-//void loop()
-//{
-//  /*
-//  if (digitalRead(BUTTON_PIN) == LOW)
-//  { // Check if button has been pressed
-//    while (digitalRead(BUTTON_PIN) == LOW)
-//      ; // Wait for button to be released
-//
-//    digitalWrite(LED_PIN, HIGH); // Turn on LED
-//    requestURL(hostDomain, hostPort); // Connect to server
-//    digitalWrite(LED_PIN, LOW); // Turn off LED
-//  }
-//  */
-//  
-//}
-
-//void connectToWiFi(const char * ssid, const char * pwd)
-//{
-//  int ledState = 0;
-//
-//  printLine();
-//  Serial.println("Connecting to WiFi network: " + String(ssid));
-//
-//  WiFi.begin(ssid, pwd);
-//
-//  while (WiFi.status() != WL_CONNECTED) 
-//  {
-//    // Blink LED while we're connecting:
-//    digitalWrite(LED_PIN, ledState);
-//    ledState = (ledState + 1) % 2; // Flip ledState
-//    delay(500);
-//    Serial.print(".");
-//  }
-//
-//  Serial.println();
-//  Serial.println("WiFi connected!");
-//  Serial.print("IP address: ");
-//  Serial.println(WiFi.localIP());
-//}
-//
-//void requestURL(const char * host, uint8_t port)
-//{
-//  printLine();
-//  Serial.println("Connecting to domain: " + String(host));
-//
-//  // Use WiFiClient class to create TCP connections
-//  WiFiClient client;
-//  if (!client.connect(host, port))
-//  {
-//    Serial.println("connection failed");
-//    return;
-//  }
-//  Serial.println("Connected!");
-//  printLine();
-//
-//  // This will send the request to the server
-//  client.print((String)"GET / HTTP/1.1\r\n" +
-//               "Host: " + String(host) + "\r\n" +
-//               "Connection: close\r\n\r\n");
-//  unsigned long timeout = micros();
-//  while (client.available() == 0) 
-//  {
-//    if (micros() - timeout > 5000) 
-//    {
-//      Serial.println(">>> Client Timeout !");
-//      client.stop();
-//      return;
-//    }
-//  }
-//
-//  // Read all the lines of the reply from server and print them to Serial
-//  while (client.available()) 
-//  {
-//    String line = client.readStringUntil('\r');
-//    Serial.print(line);
-//  }
-//
-//  Serial.println();
-//  Serial.println("closing connection");
-//  client.stop();
-//}
-//
-//void printLine()
-//{
-//  Serial.println();
-//  for (int i=0; i<30; i++)
-//    Serial.print("-");
-//  Serial.println();
-//}
